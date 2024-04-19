cf19c5e0d64c2183064a615e1a29162e75082f8c
Bremigo <161054292+Bremigo@users.noreply.github.com>
2024-04-19 15:27:18 +0200

    Update openai.py

--- chatbot/openai.py
+++ chatbot/openai.py
@@ -1,4 +1,4 @@
 import os
 
-OPENAI_KEY = sk-proj-PLuWzv7UnlkZhueLa40oT3BlbkFJtvvjmi0Tsz4plOldOMz1['OPENAI_KEY']
+OPENAI_KEY = "sk-proj-PLuWzv7UnlkZhueLa40oT3BlbkFJtvvjmi0Tsz4plOldOMz1"
 OPENAI_MODEL = "gpt-3.5-turbo-16k"
